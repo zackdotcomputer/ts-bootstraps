a491cfc2666d22406ed79b0d56fd832a8f412301
Zack Sheppard <zack@zacksheppard.com>
2021-06-20 13:19:45 +0100

    Fix lint and test for next clean

--- next/.eslintrc.js
+++ next/.eslintrc.js
@@ -1,51 +1,48 @@
 module.exports = {
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json"
   },
   extends: [
-    'airbnb-typescript',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:prettier/recommended',
-    'plugin:react/recommended',
-    'prettier',
+    "airbnb-typescript",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+    "prettier"
   ],
   env: {
     es6: true,
     browser: true,
     jest: true,
-    node: true,
+    node: true
   },
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: "detect"
+    }
   },
-  plugins: ['prettier'],
+  plugins: ["prettier"],
   rules: {
-    'import/extensions': ['off'],
-    'no-else-return': ['off'],
-    'react/react-in-jsx-scope': ['off'],
-    'react/prop-types': [0],
-    'react/require-default-props': [0],
-    'react/jsx-one-expression-per-line': [0],
-    'react/jsx-wrap-multilines': [0],
-    'react/jsx-props-no-spreading': [0],
-    'no-param-reassign': [
-      'error',
-      { props: true, ignorePropertyModificationsFor: ['state'] },
-    ],
-    'jsx-a11y/anchor-is-valid': [
-      'error',
+    "import/extensions": ["off"],
+    "no-else-return": ["off"],
+    "react/react-in-jsx-scope": ["off"],
+    "react/prop-types": [0],
+    "react/require-default-props": [0],
+    "react/jsx-one-expression-per-line": [0],
+    "react/jsx-wrap-multilines": [0],
+    "react/jsx-props-no-spreading": [0],
+    "no-param-reassign": ["error", { props: true, ignorePropertyModificationsFor: ["state"] }],
+    "jsx-a11y/anchor-is-valid": [
+      "error",
       {
-        components: ['Link', 'LinkTo'],
-      },
+        components: ["Link", "LinkTo"]
+      }
     ],
-    'no-console': [
+    "no-console": [
       2,
       {
-        allow: ['warn', 'error'],
-      },
-    ],
-  },
-}
+        allow: ["warn", "error"]
+      }
+    ]
+  }
+};
